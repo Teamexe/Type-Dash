dcb011c02011fd2e2641b8fbf32ad63f152237aa
Anirudh Sharma <126901961+anisharma07@users.noreply.github.com>
2024-06-17 00:30:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { join } = require("node:path");
 const app = express();
 const server = createServer(app);
 const { Server } = require("socket.io");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const { get } = require("http");
 const io = new Server(server);
 
